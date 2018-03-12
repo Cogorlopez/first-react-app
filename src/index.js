28f1be59f42832a755711c3cb20263a783c39f70
Cogorlopez <Cogorlopez@gmail.com>
2018-03-11 21:30:34 -0500

    App has been started. Search box and video list have been started.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import SearchBar from './components/searchBar';
+import VideoList from './components/videoList';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyAwXlG4rA7SeDwy2fJrz3vdLjNsgfxFXFg';
+
+//Create a new component. This componet should produce some html
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'spaffords'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
