bd2a7fddc640aa44646f9b92528015d6ce3a8e60
Cogorlopez <Cogorlopez@gmail.com>
2018-04-26 09:25:19 -0500

    Functional video browser!

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -13,14 +13,15 @@ class SearchBar extends React.Component {
       <div className="search-bar">
         <input
           value= {this.state.term}
-          onChange={e => this.setState({term: e.target.value})} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
   }
 
-  // onInputChange(e) {
-  //   console.log(e.target.value);
-  // }
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchBar';
@@ -15,18 +16,26 @@ class App extends React.Component {
       videos: [],
       selectedVideo: null
     };
+    
+    //this.videoSearch = this.videoSearch.bind(this);
+    this.videoSearch('spafford');
+  }
 
-    YTSearch({key: API_KEY, term: 'spafford'}, (videos) => {
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
     });
   }
+
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
